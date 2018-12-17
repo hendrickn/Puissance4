57ec0b80d1fcfe374b81e3800fc76bde655ceddd
2018hendrickn <niels.hendrickx@student-cs.fr>
2018-12-17 10:30:36 +0100

    alpha-beta bis pls

--- ai_player.py
+++ ai_player.py
@@ -12,35 +12,42 @@ class AIPlayer(Player):
 
 
     def getColumn(self, board):
-        return maxvalue(board,5)
+        return maxvalue(board,5)[1]
 
 
-def h(board) :
-    return 0
+def h(board,action) :
+    score=0
+    board_copy=deepcopy(board)
+    for s in board_copy.getPossibleColumns() :
+        L=board_copy.getCol(s).sort()
+        F=[i for i in L if i>0]
 
 
-def minvalue(board,action,prof,alpha=-1*inf,beta=inf) :
+
+
+
+def minvalue(board,prof,action=0,alpha=-1*inf,beta=inf) :
     if prof==0 :
         return h(action)
     for s in board.getPossibleColumns() :
         board_bis=deepcopy(board)
         board_bis.play(1,s)
-        if minvalue(board,prof-1,alpha,beta)>=alpha :
-            alpha=minvalue(board,prof-1,alpha,beta)
+        if minvalue(board_bis,prof-1,s,alpha,beta)>=alpha :
+            alpha=minvalue(board_bis,prof-1,s,alpha,beta)
             choice=s
         if alpha>=beta :
             return beta
     return alpha,choice
 
 
-def maxvalue(board,action,prof,alpha=-1*inf,beta=inf) :
+def maxvalue(board,prof,action=0,alpha=-1*inf,beta=inf) :
     if prof==0 :
         return h(action)
     for s in board.getPossibleColumns() :
         board_bis=deepcopy(board)
         board_bis.play(-1,s)
-        if maxvalue(board_bis,s,prof-1,alpha,beta)>=alpha :
-            alpha=maxvalue(board_bis,s,prof-1,alpha,beta)
+        if maxvalue(board_bis,prof-1,s,alpha,beta)>=alpha :
+            alpha=maxvalue(board_bis,prof-1,s,alpha,beta)
             choice=s
         if alpha>=beta :
             return beta
