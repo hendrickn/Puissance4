050378833e04127d3f4fd6c5ab6dd45b10bd2a0c
2018hendrickn <niels.hendrickx@student-cs.fr>
2018-12-17 09:52:26 +0100

    alpha-beta

--- ai_player.py
+++ ai_player.py
@@ -1,19 +1,49 @@
 from player import Player
+from math import *
+from copy import *
 
 
 class AIPlayer(Player):
     """This player should implement a heuristic along with a min-max and alpha
     beta search to """
-	
+
     def __init__(self):
         self.name = "Timo Maszewski & Niels Hendrickx"
 
-    
+
     def getColumn(self, board):
-         # TODO(student): implement this!
-        
-        return 0
+        return maxvalue(board,5)
 
 
 def h(board) :
     return 0
+
+
+def minvalue(board,prof,alpha=-1*inf,beta=inf) :
+    if prof==0 :
+        return h(board)
+    for s in board.getPossibleColumns() :
+        board_bis=deepcopy(board)
+        board_bis.play(1,s)
+        if minvalue(board,prof-1,alpha,beta)>=alpha :
+            alpha=minvalue(board,prof-1,alpha,beta)
+            choice=s
+        if alpha>=beta :
+            return beta
+    return alpha,choice
+
+
+def maxvalue(board,prof,alpha=-1*inf,beta=inf) :
+    if prof==0 :
+        return h(board)
+    for s in board.getPossibleColumns() :
+        board_bis=deepcopy(board)
+        board_bis.play(-1,s)
+        if maxvalue(board,prof-1,alpha,beta)>=alpha :
+            alpha=maxvalue(board,prof-1,alpha,beta)
+            choice=s
+        if alpha>=beta :
+            return beta
+    return alpha,choice
+
+
