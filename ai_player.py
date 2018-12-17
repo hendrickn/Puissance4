407848667920c483dd8510c2736233fdf846da84
2018hendrickn <niels.hendrickx@student-cs.fr>
2018-12-17 09:06:29 +0100

    first commit

---
+++ ai_player.py
@@ -0,0 +1,19 @@
+from player import Player
+
+
+class AIPlayer(Player):
+    """This player should implement a heuristic along with a min-max and alpha
+    beta search to """
+	
+    def __init__(self):
+        self.name = "Timo Maszewski & Niels Hendrickx"
+
+    
+    def getColumn(self, board):
+         # TODO(student): implement this!
+        
+        return 0
+
+
+def h(board) :
+    return 0
